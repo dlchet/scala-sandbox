3b35703db167acadc18400f893a229de096ad10b
daniel chetlin <chetlin.d@gmail.com>
2022-04-24 10:01:37 -0400

    chore: sbt new scala/scala-seed.g8
    feat: added scalacheck and an example specification
    chore: ran source formatter and generated a .scalafmt.conf

---
+++ src/test/scala/example/StringSpecification.scala
@@ -0,0 +1,10 @@
+package example
+
+import org.scalacheck.Properties
+import org.scalacheck.Prop.forAll
+
+object StringSpecification extends Properties("String") {
+  property("startsWith") = forAll { (a: String, b: String) =>
+    (a + b).startsWith(a)
+  }
+}
